29d5eee8429e2ec63b54fafdba7d7d4302079a3f
xuwen <xuwen@juzix.net>
2021-09-15 15:46:40 +0800

    replace non release version

--- bin/list.js
+++ bin/list.js
@@ -1,15 +1,15 @@
 
 var soljsonSources = [
-  "soljson-v0.8.6+commit.915f2fcc.js",
-  "soljson-v0.7.6+commit.6cb3c108.js",
-  "soljson-v0.6.12+commit.8946da9f.js",
+  "soljson-v0.8.6+commit.d0f402ce.js",
+  "soljson-v0.7.6+commit.c726b9c0.js",
+  "soljson-v0.6.12+commit.8d65dee4.js",
   "soljson-v0.5.17+commit.66b1543b.js",
   "soljson-v0.4.26+commit.5eff4511.js"
 ];
 var soljsonReleases = {
-  "0.8.6": "soljson-v0.8.6+commit.915f2fcc.js",
-  "0.7.6": "soljson-v0.7.6+commit.6cb3c108.js",
-  "0.6.12": "soljson-v0.6.12+commit.8946da9f.js",
+  "0.8.6": "soljson-v0.8.6+commit.d0f402ce.js",
+  "0.7.6": "soljson-v0.7.6+commit.c726b9c0.js",
+  "0.6.12": "soljson-v0.6.12+commit.8d65dee4.js",
   "0.5.17": "soljson-v0.5.17+commit.66b1543b.js",
   "0.4.26": "soljson-v0.4.26+commit.5eff4511.js"
 };
